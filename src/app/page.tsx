807ead1b38a798bb7a72f423ee158551e0152a31
Darián López Utra <daroxs95@gmail.com>
2023-09-26 05:48:50 -0400

    Deploy to cloudflare edge runtime
    Fix CD build step
    
    Add write permissions to CD (for gh deployments)
    
    Set deploy as production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { endpoints } from 'base/config';
 
-export default function Home() {
-  return <main className={styles.main}>TODO</main>;
+export const runtime = 'edge';
+
+async function getData() {
+  const res = await fetch(endpoints.api.students);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  return <main className={styles.main}>{data.name}</main>;
 }
